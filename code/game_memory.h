259516fa1e78904b7f4dde2f6fe5cd3e3e305c63
Groffa <jens@pushingcows.se>
2015-11-01 19:46:21 +0100

    Wrong size of pointer, switched to byted sized (incrementing would be wrong)

--- code/game_memory.h
+++ code/game_memory.h
@@ -17,14 +17,14 @@ Allocate(gamememory_t *GameMemory, uint Size)
 {
     // TODO: check memory sizes, bounds
     
-    uint *Address = (uint *)(GameMemory->Data);
-    uint *LastAddress = (uint *)(Address + GameMemory->Size);
+    char *Address = (char *)(GameMemory->Data);
+    char *LastAddress = (Address + GameMemory->Size);
     memoryprefix_t *Prefix = 0;
     bool Found = false;
     while (!Found && Address < LastAddress) {
         Prefix = (memoryprefix_t *)Address;
         if (Prefix->Taken) {
-            Address = (uint *)(((char *)Address) + Prefix->Size + sizeof(memoryprefix_t));
+            Address = ((char *)Address) + Prefix->Size + sizeof(memoryprefix_t);
         } else {
             Found = true;
         }
@@ -34,7 +34,7 @@ Allocate(gamememory_t *GameMemory, uint Size)
     if (Found && Prefix) {
         Prefix->Taken = true;
         Prefix->Size = Size;
-        NewSpace = (void *)(((char *)Address) + sizeof(memoryprefix_t));
+        NewSpace = (void *)(Address + sizeof(memoryprefix_t));
     }
     return NewSpace;
 }
@@ -47,7 +47,6 @@ Deallocate(gamememory_t *GameMemory, void *Memory)
     
     memoryprefix_t *Prefix = (memoryprefix_t *)(((char *)Memory) - sizeof(memoryprefix_t));
     Prefix->Taken = false;
-    Prefix->Size = 0;   // Not needed, will be overwritten
 }
 
 #define GAME_MEMORY_H
